1ab316eea7243a087477626f096f53deef0dde07
yasuper8 <ysuper8@gmail.com>
2016-09-17 21:07:18 -0700

    add font

--- app.js
+++ app.js
@@ -4,25 +4,11 @@
 console.log("linked!");
 $(document).ready(function(){
 
-//Can pick amount of pairs of cards in the game
-//Can choose difficulty of the game
-// //the amount of time revealing the placed cards
-//
 
-
-//constructors
-
-  //Game boards
-      //make board
-      //reset board
-      //check cards
-      //findWinner
-      //add player
-
-function MakePlayer(name){
-    this.name = name;
-    this.scoreTracker = 0;
-  }
+// function MakePlayer(name){
+//     this.name = name;
+//     this.scoreTracker = 0;
+//   }
 
 function Card(number){
   this.number = number;
@@ -30,20 +16,21 @@ function Card(number){
 }
 
 function GameBoard(){
-  this.cardNames = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  this.cardNames = ["one", "one", "two", "two", "three", "three", "four", "four", "five", "five", "six", "six", "seven", "seven", "eight", "eight", "nine", "nine", "ten", "ten", "J", "J", "Q", "Q", "K", "K"];
+  this.cardFlipped = 0;
 
-  var cards = [];
+  var deckOfCards = [];
   var shuffledCards = [];
-  var newBoard = '<div class="card"></div>';
-
+  
   for(var i = 0; i < this.cardNames.length; i++){
-    cards.push(new Card(this.cardNames[i]));
-    $("#wrapper").append(newBoard);
+    deckOfCards.push(new Card(this.cardNames[i]));
   }
-  for(var j = 0; j <= cards.length; j++){
-    var randomNumber = Math.floor(Math.random() * cards.length) + 1;
-    shuffledCards.push(cards[randomNumber]);
+  for(var j = 0; j <= deckOfCards.length; j++){
+    var randomNumber = Math.floor(Math.random() * deckOfCards.length);
+    shuffledCards.push(deckOfCards[randomNumber]);
+    $("#game-board").append('<div class="card" id="card ' + deckOfCards[randomNumber].number + '" onclick="#"></div>');
   }
+  console.log(shuffledCards);
   return shuffledCards;
 }
 
@@ -51,9 +38,71 @@ function GameBoard(){
 
 
 
+// function flipCard(){
+ //  var cardPairPicked = [];
+ //  var cardValue = document.getAttribute("card-data");
+ //  cardPairPicked.push(cardValue);
+ //  if(cardValue.html === "" & cardPairPicked.length < 2){
+ //    cardValue.style.background = "#FFF";
+ //    cardValue.html = cardValue;
+ //    console.log(cardValue);
+ //  }
+ // }
+// 
+// function doTwice(func){
+//   for(var i = 1; i <= 2; i++){
+//     func();
+//   }
+// }
+// var removeCards = function() {
+//   var gameBoard = document.getElementById('game-board');
+//  gameBoard.parentNode.removeChild(gameBoard);
+// };
 
 
-});
+
+
+// var htmlOutput = '<div  id=" card ' +  deckOfCards[randomNumber].number + '"><div class="front">?</div><div class="back">' + this.number + '</div></div>';
+
+// <div id="card"> 
+//   <div class="front"> 
+//     Front content
+//   </div> 
+//   <div class="back">
+//     Back content
+//   </div> 
+// </div>
+// Then, call jQuery flip() on it:
+
+// $("#card").flip();
+
+
+ // function isTwoCards(arr){
+ //  var cardPairPicked = [];
+ //  cardPairPicked.push(this.getAttribute("card-data"));
+ //  console.log(this.getAttribute("card-data"));
+ //  for(var i = 0; i <= arr.length; i++){
+ //    if (this.getAttribute("card-data") === arr[i]) {
+ //    this.innerHTML = '<div class="card" card-data=' + arr[i] +  '><h1 id="cardTitle">' + arr[i] + '</h1></div>';
+ //      // $(".flipped").attr("disabled", "disabled");
+ //      // document.getElementByClassName('.flipped').disabled = true;
+ //      // $("#originalC").hide();
+ //      } 
+ //      // if(cardsInPlay.length === 2){
+ //      // isMatch(cardsInPlay);
+ //      // cardsInPlay = [];
+ //    }
+ //  }
+  
+
+
+
+}); //end .deady
+
+
+
+
+
 
 // var makeBorad = function(){
 //   var newBoard = $.create("div");
